22852e0728983a3813af9fdc9c0a172fe510bb0e
Yeonjung Choi <40679419+YeonjungChoi97@users.noreply.github.com>
2022-05-04 17:39:19 +0900

    Update 표 편집.py

--- python/표 편집.py
+++ python/표 편집.py
@@ -1,5 +1,6 @@
+#very difficult, had to recap on dllist for around 2-3 hrs
+#took ~ 3.5 ish hrs
 #link: https://programmers.co.kr/learn/courses/30/lessons/81303
-## runtime errors :/ code efficiency apparently 0
 
 # Create the node class
 class Node:
@@ -7,63 +8,21 @@ class Node:
         self.data = data
         self.next = None
         self.prev = None
-        
-# Create the doubly linked list class
-class doubly_linked_list:
-    def __init__(self):
-        self.head = None
-
-    def append(self, NewVal):
-        NewNode = Node(NewVal)
-        NewNode.next = None
-        if self.head is None:
-            NewNode.prev = None
-            self.head = NewNode
-            return
-        last = self.head
-        while (last.next is not None):
-            last = last.next
-        last.next = NewNode
-        NewNode.prev = last
-        return
-    
-    def final(self, node):
-        str_final = ""
-        while (node is not None):
-            
-            str_final = str_final + node.data 
-            last = node
-            node = node.next
-        return str_final
-    
-    
-        
-
     
 def solution(n, k, cmd):
     
-    dlist = doubly_linked_list()
-    first_node = "O"
-    dlist.append(first_node)
-    current_node = first_node
+    node_list = [Node("O") for _ in range(n)]
+    for i in range(1,n):
+        node_list[i-1].next = node_list[i]
+        node_list[i].prev = node_list[i-1]
+        
+        
     deleted_node = []
+    current_node = node_list[k]
     
-    #initialising linked list
-    while n > 1:
-        next_node = "O"
-        dlist.append(next_node)
-        current_node = next_node
-        n -= 1 
-    
-    
-    #place the node to k position
-    current_node = dlist.head
-
-    while k > 0:
-        current_node = current_node.next
-        k -= 1
     
     for i in cmd:
+        
         i = i.split(" ")
         if i[0] == "U":
             for i in range(int(i[1])):
@@ -77,12 +36,13 @@ def solution(n, k, cmd):
 
             upper = current_node.prev
             lower = current_node.next
-            if lower:
+            if upper:
                 upper.next = lower
+
+            if lower:
                 lower.prev = upper
                 current_node = lower
             else:
-                upper.next = lower
                 current_node = upper
         else:
             undo = deleted_node.pop()
@@ -94,15 +54,10 @@ def solution(n, k, cmd):
             if lower:
                 lower.prev = undo
 
-    while len(deleted_node) > 0:
-        undo = deleted_node.pop()
-        upper = undo.prev
-        lower = undo.next
-        upper.next = undo
-        lower.prev = undo
-
-    answer = dlist.final(dlist.head)
-
+    answer = ""
+    
+    for i in range(len(node_list)):
+        answer += node_list[i].data
     
     
     return answer
